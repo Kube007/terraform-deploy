2ae0f2f7400859217acc7a7382644e7132f58ad3
Kube007 <160099025+Kube007@users.noreply.github.com>
2024-10-15 01:30:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "my-test-cr7 "
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+
 
